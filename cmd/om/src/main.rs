674b42e1292db9513b31a3501d126d860e1472a9
Gabriel Rosenhouse <grosenhouse@vmware.com>
2022-08-22 17:01:39 -0700

    store timeseries data too

--- cmd/om/src/main.rs
+++ cmd/om/src/main.rs
@@ -59,26 +59,37 @@ struct MetricSample {
     pub labels: Labels,
 }
 
-struct Index {
-    pub timeseries_name_to_id: HashMap<String, u32>,
-    pub num_timeseries: u32,
-    pub single_label_bitmaps: BTreeMap<(String,String), RoaringBitmap>,
+struct Sample {
+    pub timestamp_ms: i64,
+    pub value: f64,
 }
 
-impl Index {
-    pub fn new() -> Index {
-        Index {
+struct DB {
+    pub timeseries_name_to_id: HashMap<String, u32>,
+    pub num_timeseries: u32,
+    pub single_label_bitmaps: BTreeMap<(String, String), RoaringBitmap>,
+    pub naive_column_store: Vec<Vec<Sample>>,
+}
+
+impl DB {
+    pub fn new() -> DB {
+        DB {
             timeseries_name_to_id: HashMap::new(),
             num_timeseries: 0,
-            single_label_bitmaps : BTreeMap::new(),
+            single_label_bitmaps: BTreeMap::new(),
+            naive_column_store: Vec::new(),
         }
     }
 
     // gets existing timeseries id, or creates a new timeseries id, for a
     // metric sample with the provided labels.  Updates inverted indexes if necessary.
-    pub fn get_timeseries_id(&mut self, labels: Labels) -> u32 {
+    fn get_timeseries_id(&mut self, labels: Labels) -> u32 {
         let l = labels.0;
-        let full_name = (&l).into_iter().map(|(k, v)| k.to_owned() + "=" + &v).reduce(|acc, n| acc + "|" + &n) .unwrap();
+        let full_name = (&l)
+            .into_iter()
+            .map(|(k, v)| k.to_owned() + "=" + &v)
+            .reduce(|acc, n| acc + "|" + &n)
+            .unwrap();
 
         let timeseries_id = *(self
             .timeseries_name_to_id
@@ -86,6 +97,7 @@ impl Index {
             .or_insert_with(|| {
                 let id = self.num_timeseries;
                 self.num_timeseries += 1;
+                self.naive_column_store.push(Vec::new());
                 id
             }));
 
@@ -97,38 +109,47 @@ impl Index {
         timeseries_id
     }
 
+    pub fn ingest(&mut self, s: MetricSample) -> u32 {
+        let sample = Sample {
+            timestamp_ms: s.timestamp_ms,
+            value: s.value,
+        };
+        let ts_id = self.get_timeseries_id(s.labels);
+        self.naive_column_store[ts_id as usize].push(sample);
+        ts_id
+    }
+
     pub fn search(&self) {
         let key_range_start = ("pod".to_owned(), "abc".to_owned());
         let key_range_end = ("pod".to_owned(), "abd".to_owned());
-        let mut n = 0;
         let start_time = std::time::Instant::now();
-        for (key, value) in self.single_label_bitmaps.range(key_range_start .. key_range_end) {
-            // println!("{:?}: {:?}", key, &value.len());
-            n += 1
-        }
+        let matches = self
+            .single_label_bitmaps
+            .range(key_range_start..key_range_end)
+            .fold(0u64, |acc, (_, bitmap)| acc + bitmap.len());
         let elapsed = start_time.elapsed();
-        println!("found {} matching in {} seconds", n, elapsed.as_secs_f32());
+        println!(
+            "found {} matching in {} seconds",
+            matches,
+            elapsed.as_secs_f32()
+        );
     }
 }
 
-
 fn build(source_file: String, _index_file: String) -> Result<(), Box<dyn std::error::Error>> {
-    let mut index = Index::new();
+    let mut db = DB::new();
     let source_file = File::open(source_file)?;
     let source_reader = BufReader::new(source_file);
-    let metrics = serde_json::Deserializer::from_reader(source_reader)
-        .into_iter::<MetricSample>()
-        .map(|x| x.unwrap());
-    for m in metrics {
-        let id = index.get_timeseries_id(m.labels);
+    for m in serde_json::Deserializer::from_reader(source_reader).into_iter::<MetricSample>() {
+        let m = m.unwrap();
+        let id = db.ingest(m);
         if (id + 1) % 100000 == 0 {
             println!("timeseries {}", id + 1);
         }
     }
-    index.search();
+    db.search();
     Ok(())
 }
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cmdline: CmdLine = CmdLine::parse();
